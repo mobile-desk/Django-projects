93450129c89f31c2103cd52ab1d632e38ab42264
Mobile Desk <63956765+mobile-desk@users.noreply.github.com>
2022-07-04 22:23:28 +0100

    Update settings.py

--- contact/phone/phone/settings.py
+++ contact/phone/phone/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u5!_-22i5$=e^vl(9do0-s&p!rtecci5pc!4xmrimr4((^d+qq'
+SECRET_KEY = ' '
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
